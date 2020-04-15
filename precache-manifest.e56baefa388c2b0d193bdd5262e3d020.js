c8b886f7464b4ebfe05d871344ca7433321b24c7
Jayjaybunce <jayjaybunce>
2020-04-15 14:16:11 -0600

    Updates

---
+++ precache-manifest.e56baefa388c2b0d193bdd5262e3d020.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2a6c99ebcdbe60553d097e25f5d45a1",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "28775e520c4170eefe55",
+    "url": "/project-4-assessment/static/css/main.0e4855d8.chunk.css"
+  },
+  {
+    "revision": "bf9faed5984ec60f7781",
+    "url": "/project-4-assessment/static/js/2.36c0ca1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.36c0ca1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28775e520c4170eefe55",
+    "url": "/project-4-assessment/static/js/main.325df2d7.chunk.js"
+  },
+  {
+    "revision": "bb8b828653ad1446ab40",
+    "url": "/project-4-assessment/static/js/runtime-main.58f97c80.js"
+  }
+]);
